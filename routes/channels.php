b21666436509e33bfeefb2148768064ffd7bf781
Mifaki <agustianto.d19@gmail.com>
2025-07-17 23:02:55 +0700

    feat: strict channel to only admin role

--- routes/channels.php
+++ routes/channels.php
@@ -6,6 +6,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('dashboard-updates', function () {
-    return true;
+Broadcast::channel('dashboard-updates', function ($user) {
+    return $user->hasAnyRole(['Super Admin', 'Admin OPD']);
 });
